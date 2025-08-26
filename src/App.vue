1383de0c50e6d717df06676fcec0fe17d7f52134
QNquenan <qn2987271942@outlook.com>
2025-08-26 23:05:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <main-card></main-card>
+  </main>
+</template>
+
+<script setup>
+import MainCard from "./views/MainCard.vue";
+</script>
+
+<style>
+@import url(assets/css/App.css);
+</style>
---
+++ src/views/MainCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mainCard">
+    <div class="avatar" :emjoi="config.emjoi">
+      <img :src="config.avatarUrl" alt="" />
+    </div>
+
+    <div class="sayHi">
+      <h1>
+        Hi, I'm
+        <span class="name" :data-text="config.name">
+          {{ config.name }}
+        </span>
+      </h1>
+
+      <div class="infoTags">
+        <div class="tag">{{ config.infoTags.sex }}</div>
+        <div class="tag">{{ config.infoTags.province }}</div>
+        <div class="tag">{{ config.infoTags.school }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import config from "../config/config.json";
+</script>
+
+<style>
+@import url(../assets/css/MainCard.css);
+</style>
